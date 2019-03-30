5a7bf0cef4ae91fa06306e06c3e5ead386ce6f44
Emmett Shaughnessy <emmett0502@gmail.com>
2019-03-30 12:01:06 -0500

    Start Time

--- InLine/InLine/ViewController.swift
+++ InLine/InLine/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
-import MapKit
 
-import CoreLocation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        timeRemaining.text = "\(hours) hours and \(minuets) minutes"
+    }
+    
+    var timeSetup1 = 1
+    var hours = 1
+    var minuets = 1
+    
+    @IBOutlet weak var queueNumber: UILabel!
+    @IBOutlet weak var timeRemaining: UILabel!
+    
+    func findQueueTime() {
+        let queue:Double = Double(queueNumber!.text!)!
+        timeSetup1 = Int(100+(queue * 0.3))
+        
         
     }
+    
     
     
 }
