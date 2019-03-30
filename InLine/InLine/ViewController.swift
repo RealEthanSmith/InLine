b3ab312feb0af9e0169382cf34e7738fe83ea3fe
Emmett Shaughnessy <emmett0502@gmail.com>
2019-03-30 09:56:17 -0500

    Base Xcode Project

---
+++ InLine/InLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InLine
+//
+//  Created by Emmett Shaughnessy on 3/30/19.
+//  Copyright © 2019 Emmett Shaughnessy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
